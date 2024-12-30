844ba6f94ccefee1655a4106e4e46e43a1ac793f
Ss0Mae <ssomae@protonmail.ch>
2024-12-30 16:50:54 +0900

    [Test]: Connection Test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT;
   await app.listen(process.env.PORT);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
