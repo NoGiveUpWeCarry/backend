97e88cba2a953f12fca2860cb0d3ff736bbc9e00
Ss0Mae <ssomae@protonmail.ch>
2025-01-06 21:33:41 +0900

    [Feat] UserModule 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from '@modules/auth/auth.module';
-
+import { UserModule } from '@modules/user/user.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // 환경 변수를 글로벌하게 사용할 수 있도록 설정
     }),
     AuthModule, // Auth 모듈 추가
+    UserModule,
   ],
 })
 export class AppModule {}
