0806e4dab5809a01af7b693e06494486e7937091
Ss0Mae <ssomae@protonmail.ch>
2025-01-06 18:43:59 +0900

    [Feat] Constant 에러 메세지 구현

---
+++ src/common/constants/error-messages.ts
@@ -0,0 +1,6 @@
+export const ERROR_MESSAGES = {
+  UNAUTHORIZED: '인증되지 않은 상태입니다 토큰을 확인해주세요.',
+  USER_NOT_FOUND: '존재하지 않는 유저입니다.',
+  VALIDATION_FAILED: '입력값에 대한 유효성이 검증되지 않았습니다.',
+  INTERNAL_SERVER_ERROR: '서버 내부 오류입니다 잠시 후 다시 시도해주세요.',
+};
