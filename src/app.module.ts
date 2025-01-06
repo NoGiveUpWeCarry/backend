477e681bd49bef8ac58350a2932bacca490c97fc
Ss0Mae <ssomae@protonmail.ch>
2025-01-06 18:40:52 +0900

    [Feat] main Module에 auth Module 연결 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from '@src/app.controller';
-import { AppService } from '@src/app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from '@modules/auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 환경 변수를 글로벌하게 사용할 수 있도록 설정
+    }),
+    AuthModule, // Auth 모듈 추가
+  ],
 })
 export class AppModule {}
