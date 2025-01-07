7ec15eab2ee3f879a454e1aed5bd792de55848bf
Dong Hyun <decidere@naver.com>
2025-01-07 19:41:41 +0900

    [Feat] 채팅방 생성 구현

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@src/prisma/prisma.service';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  // 채팅방 생성
+  async createRoom(id: number, name: string) {
+    const data = { id, name };
+    await this.prisma.room.create({ data });
+  }
+}
