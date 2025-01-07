745e037f45c008fd1878a1268ef42cf80f071504
ssomae <80831228+Ss0Mae@users.noreply.github.com>
2025-01-07 17:16:34 +0900

    [Feature] 소셜 로그인 로직 변경, 역할 선택 API 구현 (#8)
    * [Feat] JWT Strategy 리턴값 변경
    
    * [Feat] CallBack URI 변경
    
    * [Feat] Role 선택 API 구현
    
    * [Feat] 소셜 로그인 로직 변경
    
    * [Feat] CORS 설정

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from '@common/filters/http-exception.filter';
 import { config } from 'dotenv';
+import * as express from 'express';
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors({
+    origin: 'http://localhost:5173' | 'https://';
+    credentials:true,
+  });
+  //app.useGlobalFilters(new HttpExceptionFilter());
 
   const port = process.env.PORT;
   await app.listen(process.env.PORT);
-  console.log(`Application is run on: http://localhost:${port}`);
 }
 bootstrap();
