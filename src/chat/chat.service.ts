dcda836c4633bc4b30fa1487338c9143583a83e5
Dong Hyun <decidere@naver.com>
2025-01-07 21:22:28 +0900

    [Feat] 채팅방 멤버 저장 구현

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -11,4 +11,20 @@ export class ChatService {
     // 생성한 채팅방 id 리턴
     return result.id;
   }
+
+  // 채팅방 멤버 저장
+  async joinRoom(roomId, userId1, userId2) {
+    await this.prisma.room_users.createMany({
+      data: [
+        {
+          room_id: roomId,
+          user_id: userId1,
+        },
+        {
+          room_id: roomId,
+          user_id: userId2,
+        },
+      ],
+    });
+  }
 }
