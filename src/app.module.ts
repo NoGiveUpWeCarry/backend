b8ee0e0333065df128f500f0b9bcdfbb13a20787
Dong Hyun <decidere@naver.com>
2025-01-07 14:18:59 +0900

    [Feat] 게이트웨이 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from '@modules/auth/auth.module';
 import { UserModule } from '@modules/user/user.module';
+import { ChatGateway } from './chat/chat.gateway';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -10,5 +11,6 @@ import { UserModule } from '@modules/user/user.module';
     AuthModule, // Auth 모듈 추가
     UserModule,
   ],
+  providers: [ChatGateway],
 })
 export class AppModule {}
