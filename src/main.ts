148080e2aabfa2bd46bf216f4299d76dd6b0b05e
Ss0Mae <ssomae@protonmail.ch>
2025-01-07 17:02:44 +0900

    [Feat] CORS 설정

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from '@common/filters/http-exception.filter';
 import { config } from 'dotenv';
+import * as express from 'express';
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5173' | 'https://';
     credentials:true,
   });
   //app.useGlobalFilters(new HttpExceptionFilter());
 
   const port = process.env.PORT;
   await app.listen(process.env.PORT);
-  console.log(`Application is run on: http://localhost:${port}`);
 }
 bootstrap();
