745044684e2a0f7f5ae4f2962731acec0515952f
Dong Hyun <decidere@naver.com>
2025-01-07 21:17:48 +0900

    [Refactor] 채팅방 생성 로직 수정

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -6,8 +6,9 @@ export class ChatService {
   constructor(private readonly prisma: PrismaService) {}
 
   // 채팅방 생성
-  async createRoom(id: number, name: string) {
-    const data = { id, name };
-    await this.prisma.room.create({ data });
+  async creatRoom() {
+    const result = await this.prisma.room.create({});
+    // 생성한 채팅방 id 리턴
+    return result.id;
   }
 }
