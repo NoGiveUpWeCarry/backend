e1ca628f45c13b3766e646745306d78039d6e521
Ss0Mae <ssomae@protonmail.ch>
2025-01-08 18:58:35 +0900

    [Refactor] 사용하지 않는 코드 정리

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,27 +1,8 @@
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
-import { ERROR_MESSAGES } from '@common/constants/error-messages';
+import { Injectable } from '@nestjs/common';
+
 import { PrismaService } from '@prisma/prisma.service';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
-  async updateUserRole(userId: number, roleId: number) {
-    if (![1, 2, 3].includes(roleId)) {
-      throw new BadRequestException(ERROR_MESSAGES.INVALID_ROLE_ID);
-    }
-
-    const user = await this.prisma.user.findUnique({ where: { id: userId } });
-    if (!user) {
-      throw new NotFoundException(ERROR_MESSAGES.USER_NOT_FOUND);
-    }
-    console.log(userId);
-    return this.prisma.user.update({
-      where: { id: userId },
-      data: { role_id: roleId },
-    });
-  }
 }
