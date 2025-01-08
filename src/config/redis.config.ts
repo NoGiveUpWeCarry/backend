3fc1488a6177d37cf2bd7a490c33de570fb83c3a
Ss0Mae <ssomae@protonmail.ch>
2025-01-08 18:57:19 +0900

    [Feat] Redis 연결 config 구현

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -1,25 +1,8 @@
-import Redis from 'ioredis';
+import { Injectable } from '@nestjs/common';
 
-export class RedisConfigService {
-  private readonly client: Redis;
-
-  constructor() {
-    this.client = new Redis({
-      host: process.env.REDIS_HOST || '127.0.0.1',
-      port: parseInt(process.env.REDIS_PORT, 10) || 6379,
-      password: process.env.REDIS_PASSWORD || undefined,
-    });
-
-    this.client.on('connect', () => {
-      console.log('Connected to Redis');
-    });
-
-    this.client.on('error', err => {
-      console.error('Redis connection error:', err);
-    });
-  }
-
-  getClient(): Redis {
-    return this.client;
-  }
+@Injectable()
+export class RedisConfig {
+  public static readonly host = process.env.REDIS_HOST || 'localhost';
+  public static readonly port = parseInt(process.env.REDIS_PORT, 10) || 6379;
+  public static readonly password = process.env.REDIS_PASSWORD || undefined;
 }
