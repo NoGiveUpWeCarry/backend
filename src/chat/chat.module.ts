9b79ff6a5867bc1c46a8a3591c163683e6fd4d8a
Dong Hyun <decidere@naver.com>
2025-01-09 16:02:14 +0900

    [Feat] Jwt 모듈 임포트

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { PrismaModule } from '@src/prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, JwtModule],
   providers: [ChatService],
 })
 export class ChatModule {}
