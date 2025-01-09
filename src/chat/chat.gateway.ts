4d2adcf4dda66853378254c93983fa460de19cd9
Dong Hyun <decidere@naver.com>
2025-01-09 17:08:49 +0900

    [Feat] 채팅방 존재 유무 확인 로직 수정

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -19,15 +19,19 @@ export class ChatGateway {
     @MessageBody() data: { channelId: string; userId: string }
   ) {
     const { channelId, userId } = data;
-    // 존재하는 채팅방인지 확인
-    const exist = await this.chatService.channelExist(channelId);
-    if (exist) return;
-
-    // 채팅방 생성
-    await this.chatService.createChannel(channelId);
 
     // userId JWT 토큰 값이라 디코딩 해야함
     const user = this.jwtService.decode(userId);
+
+    // 존재하는 채팅방인지 확인
+    const existData = await this.chatService.channelExist(
+      channelId,
+      user.user_id
+    );
+    if (existData) return;
+
+    // 채팅방 생성
+    await this.chatService.createChannel(channelId);
 
     // 채팅방 멤버 저장
     await this.chatService.joinChannel(channelId, user.user_id);
--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -6,11 +6,43 @@ export class ChatService {
   constructor(private readonly prisma: PrismaService) {}
 
   // 이미 존재하는 채팅방인지 확인
-  async channelExist(channelId) {
-    const result = await this.prisma.channel.count({
+  // 없다면 생성, 있다면 매핑 테이블 확인
+  // 매핑 테이블 유저 데이터 있다면 대화 내용 불러오기, 없다면 매핑 테이블에 유저 데이터 생성
+  async channelExist(channelId, userId) {
+    const exist = await this.prisma.channel.count({
       where: { id: channelId },
     });
-    return result;
+
+    // 존재하지 않으면 함수 종료 후 컨트롤러에서 채팅방 생성 로직 재개
+    if (!exist) return false;
+
+    // 매핑 테이블 channel_users에 유저 데이터 있는지 확인
+    const userExist = await this.prisma.channel_users.findFirst({
+      where: {
+        channel_id: channelId,
+        user_id: userId,
+      },
+    });
+
+    // 있다면 대화내용 불러오기
+    if (userExist) {
+      const message = await this.prisma.message.findMany({
+        where: {
+          channel_id: channelId,
+        },
+      });
+      return message;
+    } else {
+      // 없다면 매핑 테이블에 유저 데이터 추가
+      await this.prisma.channel_users.create({
+        data: {
+          channel_id: channelId,
+          user_id: userId,
+        },
+      });
+      const notice = `${userId}님이 채팅방에 참가했습니다`;
+      return { notice };
+    }
   }
 
   // 채팅방 생성
