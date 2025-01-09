afc27bfe5600b9a6320486bd5a3d68d63f159e5f
Ss0Mae <ssomae@protonmail.ch>
2025-01-09 17:47:01 +0900

    [Feat] Response dto 변경 구현

--- src/common/dto/response.dto.ts
+++ src/common/dto/response.dto.ts
@@ -3,17 +3,21 @@ export class ApiResponse<T> {
     code: number; // HTTP 상태 코드
     text: string; // 메시지
   };
-  data: T | null;
 
-  constructor(statusCode: number, message: string, data?: T) {
+  [key: string]: any; // 동적으로 다른 필드를 추가 가능
+
+  constructor(statusCode: number, message: string, additionalFields?: T) {
     this.message = {
       code: statusCode,
       text: message,
     };
-    this.data = data || null;
+
+    // 추가 데이터를 최상위 레벨에 병합
+    if (additionalFields) {
+      Object.assign(this, additionalFields); // `data` 키 없이 병합
+    }
   }
 }
-
 export class ErrorResponse {
   message: {
     code: number; // HTTP 상태 코드
