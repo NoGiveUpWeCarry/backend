340a0a31c08762d0a00b5efbccedcc659d0246ba
Dong Hyun <decidere@naver.com>
2025-01-09 15:52:17 +0900

    [Feat] 게이트웨이 생성

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,9 +1,27 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  MessageBody,
+} from '@nestjs/websockets';
+import { ChatService } from './chat.service';
 
 @WebSocketGateway()
 export class ChatGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  constructor(private readonly chatService: ChatService) {}
+
+  // 채팅방 참여
+  @SubscribeMessage('joinChannel')
+  async handleJoinChannel(
+    @MessageBody() data: { channelId: string; userId: string }
+  ) {
+    const { channelId, userId } = data;
+    // 존재하는 채팅방인지 확인
+    const exist = await this.chatService.channelExist(channelId);
+    if (exist) return;
+
+    // userId JWT 토큰 값이라 디코딩 해야함
+    const user = userId;
+    // 채팅방 멤버 저장
+    await this.chatService.joinChannel(channelId, userId);
   }
 }
