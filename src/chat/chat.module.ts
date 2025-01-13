169a73c690983e946f0044f95e557dc6bc5a6ff8
Dong Hyun <decidere@naver.com>
2025-01-13 13:41:36 +0900

    [Feat] chat 컨트롤러 생성

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { PrismaModule } from '@src/prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ChatController } from './chat.controller';
 
 @Module({
   imports: [PrismaModule, JwtModule],
   providers: [ChatService],
   exports: [ChatService],
+  controllers: [ChatController],
 })
 export class ChatModule {}
