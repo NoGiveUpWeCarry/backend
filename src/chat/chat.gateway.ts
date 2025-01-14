89b72b0ed59e2b93c76bff9a83a50094e367c94f
Dong Hyun <decidere@naver.com>
2025-01-14 12:58:48 +0900

    [Refactor] 유저가 참여한 채널 전체 조회 응답데이터 수정

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -4,14 +4,34 @@ import {
   MessageBody,
   WebSocketServer,
   ConnectedSocket,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
 import { ChatService } from './chat.service';
 import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({ namespace: 'chat', cors: { origin: '*' } })
-export class ChatGateway {
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(private readonly chatService: ChatService) {}
+
   @WebSocketServer() server: Server;
+
+  // 유저 소켓 접속
+  async handleConnection(client: Socket) {
+    const userId = client.handshake.query.userId;
+    client.data.userId = userId;
+
+    await this.chatService.addUserOnline(userId, client.id);
+    console.log(`User ${userId} connected with socket ID ${client.id}`);
+  }
+
+  // 유저 소켓 접속 해제
+  handleDisconnect(client: Socket) {
+    console.log('유저 연결 해제', client.data.userId);
+    // 유저가 연결 끊을 때, 온라인 유저 목록에서 삭제
+    this.onlineUsers.delete(client.data.userId);
+  }
+
   // 채팅방 참여
   @SubscribeMessage('joinChannel')
   async handleJoinChannel(
@@ -25,12 +45,26 @@ export class ChatGateway {
 
     // 채널에 유저 참여
     client.join(channelId.toString());
-    const sockets = await this.server.fetchSockets(); // 연결된 모든 소켓 가져오기
-    const targetSocket = sockets.find(socket => socket.data.userId === userId2);
-    targetSocket.join(channelId.toString());
+    console.log(`client ${client.data.userId}  ${channelId}번 채팅방 입장`);
+
+    // B 유저 온라인 여부 확인
+    const targetSocket = this.onlineUsers.get(userId2.toString());
+    if (targetSocket) {
+      // 온라인
+      targetSocket.join(channelId.toString()); // B 유저도 채널에 입장
+      targetSocket.emit('channelJoined', { channelId: channelId.toString() });
+      console.log(
+        `client ${userId2} ${channelId}번 채팅방 입장 (온라인 바로 입장)`
+      );
+    } else {
+      // 오프라인
+      // 채팅 요청에 올려둠
+      this.chatRequest.set(userId2.toString(), channelId.toString());
+      console.log(`client ${userId2} 오프라인 `);
+    }
 
     // 클라이언트에 채널id 전달
-    this.server.emit('channelJoined', { channelId });
+    client.emit('channelJoined', { channelId: channelId.toString() });
   }
 
   // 메세지 송수신
@@ -60,7 +94,7 @@ export class ChatGateway {
 
     // 전달 데이터 양식
     const sendData = { ...resData, user, date };
-
+    console.log(sendData);
     this.server.to(data.channelId).emit('message', sendData);
   }
 }
