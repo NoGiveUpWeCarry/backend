e029d5e8840057a943029a51ee6d72a366b0adc1
Dong Hyun <decidere@naver.com>
2025-01-15 16:19:22 +0900

    [Feat] 메인 페이지 조회 로직 구현

--- src/feed/feed.controller.ts
+++ src/feed/feed.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { FeedService } from './feed.service';
 
 @Controller('feed')
 export class FeedController {
   constructor(private readonly feedService: FeedService) {}
+  @Get()
+  // 메인 페이지 조회
+  async getAllFeed() {
+    return this.feedService.getAllFeeds();
+  }
 }
--- src/feed/feed.service.ts
+++ src/feed/feed.service.ts
@@ -4,4 +4,55 @@ import { PrismaService } from '@src/prisma/prisma.service';
 @Injectable()
 export class FeedService {
   constructor(private readonly prisma: PrismaService) {}
+  /** 게시글 전체 조회 / 남은 구현 과제
+   * 유저 토큰 제공 시 좋아요 여부
+   * 인기순 정렬
+   * 예외 처리
+   **/
+  async getAllFeeds() {
+    try {
+      const result = await this.prisma.feedPost.findMany({
+        include: {
+          user: {
+            select: {
+              id: true,
+              name: true,
+              email: true,
+              role: {
+                select: { name: true },
+              },
+              profile_url: true,
+            },
+          },
+          Tags: {
+            select: {
+              tag: {
+                select: { name: true },
+              },
+            },
+          },
+        },
+      });
+
+      const posts = result.map(post => ({
+        userId: post.user.id,
+        userName: post.user.name,
+        userRole: post.user.role.name,
+        userProfileUrl: post.user.profile_url,
+        title: post.title,
+        postId: post.id,
+        thumnailUrl: post.thumbnail_url,
+        content: post.content,
+        tags: post.Tags.map(v => v.tag.name),
+        commentCount: post.comment_count,
+        likeCount: post.like_count,
+        viewCount: post.view,
+        createdAt: post.created_at,
+      }));
+
+      return { posts };
+    } catch (err) {
+      return err;
+    }
+  }
 }
