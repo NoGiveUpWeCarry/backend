4d38ff923c6052808b009ee486f8eb38e6bfe999
Dong Hyun <decidere@naver.com>
2025-01-16 16:36:33 +0900

    [Refactor] 예외 처리

--- src/feed/feed.service.ts
+++ src/feed/feed.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from '@src/prisma/prisma.service';
 
 @Injectable()
@@ -6,7 +6,6 @@ export class FeedService {
   constructor(private readonly prisma: PrismaService) {}
   /** 피드 전체 조회 / 남은 구현 과제
    * 최신순 정렬
-   * 예외 처리
    **/
   async getAllFeeds(user) {
     try {
@@ -58,94 +57,134 @@ export class FeedService {
       }));
 
       return { posts };
-      // return result;
     } catch (err) {
-      return err;
+      console.log(err);
+      throw new HttpException(
+        '서버에서 오류가 발생했습니다.',
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
     }
   }
 
-  /** 피드 조회 (게시글 부분) / 남은 구현 과제
-   * 예외 처리
-   **/
+  // 피드 조회 (게시글 부분)
   async getFeed(feedId, user) {
-    const userId = user ? user.user_id : 0;
-    const result = await this.prisma.feedPost.findUnique({
-      where: { id: feedId },
-      include: {
-        Likes: {
-          where: { user_id: userId },
-        },
-        user: {
-          select: {
-            id: true,
-            name: true,
-            email: true,
-            role: {
-              select: { name: true },
-            },
-            profile_url: true,
+    try {
+      const userId = user ? user.user_id : 0;
+      const result = await this.prisma.feedPost.findUnique({
+        where: { id: feedId },
+        include: {
+          Likes: {
+            where: { user_id: userId },
           },
-        },
-        Tags: {
-          select: {
-            tag: {
-              select: { name: true },
+          user: {
+            select: {
+              id: true,
+              name: true,
+              email: true,
+              role: {
+                select: { name: true },
+              },
+              profile_url: true,
             },
           },
+          Tags: {
+            select: {
+              tag: {
+                select: { name: true },
+              },
+            },
+          },
         },
-      },
-    });
+      });
 
-    const post = {
-      userId: result.user.id,
-      userName: result.user.name,
-      userRole: result.user.role.name,
-      userProfileUrl: result.user.profile_url,
-      title: result.title,
-      postId: result.id,
-      thumnailUrl: result.thumbnail_url,
-      content: result.content,
-      tags: result.Tags.map(v => v.tag.name),
-      commentCount: result.comment_count,
-      likeCount: result.like_count,
-      viewCount: result.view,
-      createdAt: result.created_at,
-      Liked: !!result.Likes.length,
-    };
-    return { post };
+      if (!result) {
+        throw new HttpException(
+          '게시글을 찾을 수 없습니다',
+          HttpStatus.NOT_FOUND
+        );
+      }
+
+      const post = {
+        userId: result.user.id,
+        userName: result.user.name,
+        userRole: result.user.role.name,
+        userProfileUrl: result.user.profile_url,
+        title: result.title,
+        postId: result.id,
+        thumnailUrl: result.thumbnail_url,
+        content: result.content,
+        tags: result.Tags.map(v => v.tag.name),
+        commentCount: result.comment_count,
+        likeCount: result.like_count,
+        viewCount: result.view,
+        createdAt: result.created_at,
+        Liked: !!result.Likes.length,
+      };
+      return { post };
+    } catch (err) {
+      console.log(err);
+      if (err instanceof HttpException) {
+        throw err;
+      }
+
+      throw new HttpException(
+        '서버에서 오류가 발생했습니다.',
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
   }
 
+  // 피드 개별 조회 (댓글)
   async getFeedComments(feedId) {
-    const result = await this.prisma.feedComment.findMany({
-      where: {
-        post_id: feedId,
-      },
-      include: {
-        user: {
-          select: {
-            id: true,
-            name: true,
-            email: true,
-            role: {
-              select: { name: true },
+    try {
+      const result = await this.prisma.feedComment.findMany({
+        where: {
+          post_id: feedId,
+        },
+        include: {
+          user: {
+            select: {
+              id: true,
+              name: true,
+              email: true,
+              role: {
+                select: { name: true },
+              },
+              profile_url: true,
             },
-            profile_url: true,
           },
         },
-      },
-    });
+      });
 
-    const comments = result.map(c => ({
-      commentId: c.id,
-      userId: c.user.id,
-      userName: c.user.name,
-      userRole: c.user.role.name,
-      userProfileUrl: c.user.profile_url,
-      comment: c.content,
-      createdAt: c.created_at,
-    }));
+      if (!result.length) {
+        throw new HttpException(
+          '게시글을 찾을 수 없습니다',
+          HttpStatus.NOT_FOUND
+        );
+      }
 
-    return { comments };
+      const comments = result.map(c => ({
+        commentId: c.id,
+        userId: c.user.id,
+        userName: c.user.name,
+        userRole: c.user.role.name,
+        userProfileUrl: c.user.profile_url,
+        comment: c.content,
+        createdAt: c.created_at,
+      }));
+
+      return { comments };
+    } catch (err) {
+      console.log(err);
+      if (err instanceof HttpException) {
+        throw err;
+      }
+
+      throw new HttpException(
+        '서버에서 오류가 발생했습니다.',
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
   }
 
   // 게시글 좋아요 추가/제거
@@ -188,7 +227,11 @@ export class FeedService {
         return { message: '좋아요 추가 ' };
       }
     } catch (err) {
-      return err;
+      console.log(err);
+      throw new HttpException(
+        '서버에서 오류가 발생했습니다.',
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
     }
   }
 }
