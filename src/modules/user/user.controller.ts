7102bb81f4b6e100465ba429bce93b3b79369deb
Ss0Mae <ssomae@protonmail.ch>
2025-01-20 21:58:31 +0900

    [Feat] 프로젝트 수정시에도 폼데이터 사용

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -6,7 +6,6 @@ import {
   Patch,
   Post,
   Req,
-  Res,
   UseGuards,
   UseInterceptors,
   UploadedFile,
@@ -16,7 +15,6 @@ import {
   HttpException,
   HttpStatus,
   Query,
-  HttpCode,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { JwtAuthGuard } from '@modules/auth/guards/jwt-auth.guard';
@@ -127,6 +125,7 @@ export class UserController {
   }
 
   @Put('projects/:projectId')
+  @UseInterceptors(FileInterceptor('file')) // 파일 처리 인터셉터 추가
   @UpdateProjectDocs.ApiOperation
   @UpdateProjectDocs.ApiParam
   @UpdateProjectDocs.ApiBody
@@ -134,11 +133,34 @@ export class UserController {
   async updateProject(
     @Req() req,
     @Param('projectId') projectId: string,
-    @Body() projectData: any
+    @UploadedFile() file: Express.Multer.File, // 업로드된 파일 처리
+    @Body() body: any
   ) {
     const userId = req.user?.user_id;
     const numProjectId = parseInt(projectId, 10);
-    return this.userService.updateProject(userId, numProjectId, projectData);
+
+    // 이미지 업로드 처리
+    let imageUrl = null;
+    if (file) {
+      imageUrl = await this.s3Service.uploadImage(
+        userId,
+        file.buffer,
+        file.mimetype.split('/')[1], // 파일 확장자 추출
+        'pad_projects/images' // S3 저장 경로 설정
+      );
+    }
+    // body의 links 필드 처리 (JSON 문자열을 객체로 변환)
+    const projectData = {
+      ...body,
+      links: body.links ? JSON.parse(body.links) : [], // links가 있으면 파싱, 없으면 빈 배열
+    };
+
+    return this.userService.updateProject(
+      userId,
+      numProjectId,
+      projectData,
+      imageUrl
+    );
   }
 
   @Delete('projects/:projectId')
