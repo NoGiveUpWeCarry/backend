665a6de35a76308e8468193d553c7f465c4fd12b
Ss0Mae <ssomae@protonmail.ch>
2025-01-22 17:06:56 +0900

    [Feat] project Module 구현

--- src/modules/project/project.module.ts
+++ src/modules/project/project.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ProjectController } from './project.controller';
 import { ProjectService } from './project.service';
+import { AuthModule } from '@modules/auth/auth.module';
+import { UserService } from '@modules/user/user.service';
+import { PrismaService } from '@prisma/prisma.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [ProjectController],
-  providers: [ProjectService],
+  providers: [ProjectService, PrismaService],
+  exports: [UserService],
 })
 export class ProjectModule {}
