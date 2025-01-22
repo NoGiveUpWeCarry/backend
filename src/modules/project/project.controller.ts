0d14556eb21d685cbb38798b559f38e8fb3feedc
Ss0Mae <ssomae@protonmail.ch>
2025-01-22 17:06:24 +0900

    [Feat] 커넥션허브 조회 구현

--- src/modules/project/project.controller.ts
+++ src/modules/project/project.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '@modules/auth/guards/jwt-auth.guard';
+import { ProjectService } from '@modules/project/project.service';
 
-@Controller('project')
-export class ProjectController {}
+@UseGuards(JwtAuthGuard)
+@Controller('projects')
+export class ProjectController {
+  constructor(private readonly projectService: ProjectService) {}
+
+  @Get()
+  async getProjects(
+    @Query('skip') skip: number = 0,
+    @Query('limit') limit: number = 10
+  ) {
+    return this.projectService.getProjects(skip, limit);
+  }
+}
