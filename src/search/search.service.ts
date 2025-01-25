1aa79c341e0078a15f1ebb32df4dd30366fdcb81
Dong Hyun <decidere@naver.com>
2025-01-25 23:21:02 +0900

    [Feat] 피드 검색결과 조회 로직 구현

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@src/prisma/prisma.service';
 
 @Injectable()
-export class SearchService {}
+export class SearchService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  // 피드 검색결과 조회
+  async searchFeed(keyword: string, limit: number) {
+    const result = await this.prisma.feedPost.findMany({
+      where: {
+        OR: [
+          { title: { contains: keyword } },
+          { content: { contains: keyword } },
+        ],
+      },
+      include: {
+        user: true,
+        Tags: true,
+      },
+      take: limit,
+    });
+
+    return result;
+  }
+}
