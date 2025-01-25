3cae57758f1fdb767fe9101b52d1c9dd6de360df
Dong Hyun <decidere@naver.com>
2025-01-26 01:38:26 +0900

    [Feat] 모달 검색 핸들러 컨트롤러 구현

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { SearchService } from './search.service';
 
 @Controller('search')
 export class SearchController {
   constructor(private readonly searchService: SearchService) {}
+
+  // 모달 창에서 검색
+  @Get('modal')
+  async handleModalSearch(
+    @Query('keyword') keyword: string,
+    @Query('category') category: string
+  ) {
+    return await this.searchService.handleModalSearch(keyword, category);
+  }
 }
