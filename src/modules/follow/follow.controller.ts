a412150530c719c11e323e616a1791341e5c3c8a
Ss0Mae <ssomae@protonmail.ch>
2025-01-28 13:04:41 +0900

    [Feat] Follow Toggle 컨트롤러 구현

---
+++ src/modules/follow/follow.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '@src/modules/auth/guards/jwt-auth.guard';
+import { FollowService } from '@modules/follow/follow.service';
+
+@UseGuards(JwtAuthGuard)
+@Controller('follow')
+export class FollowController {
+  constructor(private readonly followService: FollowService) {}
+
+  @Post(':targetUserId')
+  async togggleFollow(
+    @Req() req: any,
+    @Param('targetUserId') targetUserId: string
+  ) {
+    const userId = req.user.user_id;
+    const numTargetUserId = parseInt(targetUserId);
+    return this.followService.toggleFollow(userId, numTargetUserId);
+  }
+}
