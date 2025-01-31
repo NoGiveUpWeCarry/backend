bfdf93d179b606363084317a1d6bd474894b32b4
Ss0Mae <ssomae@protonmail.ch>
2025-01-31 14:08:43 +0900

    [Fix] try catch

--- src/modules/notification/notification.controller.ts
+++ src/modules/notification/notification.controller.ts
@@ -9,21 +9,25 @@ export class NotificationsController {
 
   @Sse('stream')
   streamNotifications(@Req() req): Observable<any> {
-    const userId = req.user?.user_id; // 인증된 사용자 ID 가져오기
+    try {
+      const userId = req.user?.user_id; // 인증된 사용자 ID 가져오기
 
-    if (!userId) {
-      throw new Error('사용자 인증 정보가 필요합니다.');
+      if (!userId) {
+        throw new Error('사용자 인증 정보가 필요합니다.');
+      }
+
+      return this.notificationsService.notifications$.asObservable().pipe(
+        filter(notification => notification.userId === userId),
+        map(notification => ({
+          type: notification.type,
+          message: notification.message,
+          senderNickname: notification.senderNickname,
+          senderProfileUrl: notification.senderProfileUrl,
+          timestamp: new Date().toISOString(), // 알림 전송 시간 추가
+        }))
+      );
+    } catch (err) {
+      console.log(err.message);
     }
-
-    return this.notificationsService.notifications$.asObservable().pipe(
-      filter(notification => notification.userId === userId),
-      map(notification => ({
-        type: notification.type,
-        message: notification.message,
-        senderNickname: notification.senderNickname,
-        senderProfileUrl: notification.senderProfileUrl,
-        timestamp: new Date().toISOString(), // 알림 전송 시간 추가
-      }))
-    );
   }
 }
