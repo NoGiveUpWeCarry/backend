da2d51936eb3290bb098deef8d7b248c4a51f6a2
Ss0Mae <ssomae@protonmail.ch>
2025-01-31 15:15:23 +0900

    [Fix] CORS 설정 추가

--- src/modules/notification/Interceptors/notification.interceptor.ts
+++ src/modules/notification/Interceptors/notification.interceptor.ts
@@ -16,6 +16,7 @@ export class SseInterceptor implements NestInterceptor {
     res.setHeader('Cache-Control', 'no-cache, no-transform');
     res.setHeader('Connection', 'keep-alive');
     res.setHeader('Access-Control-Allow-Origin', '*'); // CORS 설정 필요시 추가
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.flushHeaders(); // 즉시 헤더를 전송하여 연결이 끊기지 않도록 함
 
     return next.handle();
