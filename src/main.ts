90bfbe0f50d90c2d6f717a1a8ba4ccbf43ab7249
Ss0Mae <ssomae@protonmail.ch>
2025-01-31 13:02:23 +0900

    [Fix] prettier 적용

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:8080', 'https://p-a-d.store'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:8080',
+      'https://p-a-d.store',
+    ],
     credentials: true,
     exposedHeaders: ['Authorization'],
   });
