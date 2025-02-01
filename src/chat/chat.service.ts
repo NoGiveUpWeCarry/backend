ccd328d4e6fc2e8eeef2526b1227a154a5b16cf6
Dong Hyun <decidere@naver.com>
2025-02-01 17:08:33 +0900

    [Feat] file-type import 문제 해결

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -3,7 +3,6 @@ import { PrismaService } from '@src/prisma/prisma.service';
 import { GetMessageDto } from './dto/getMessage.dto';
 import { SearchMessageDto } from './dto/serchMessage.dto';
 import { S3Service } from '@src/s3/s3.service';
-import { fileTypeFromBuffer } from 'file-type';
 
 @Injectable()
 export class ChatService {
@@ -521,7 +520,9 @@ export class ChatService {
 
   // 이미지 업로드
   async handleChatFiles(userId: number, file) {
+    const { fileTypeFromBuffer } = await import('file-type');
     const data = await fileTypeFromBuffer(file);
+
     const fileType = data.ext;
     const imageUrl = await this.s3.uploadImage(
       userId,
