9f1f1a9bb39f9066a741fee51928e36a36ad5c05
Ss0Mae <ssomae@protonmail.ch>
2025-02-03 14:50:46 +0900

    [Feat] 구글 배포 서버로 변경

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -24,7 +24,7 @@ export class AuthService {
           code,
           client_id: process.env.GOOGLE_CLIENT_ID,
           client_secret: process.env.GOOGLE_CLIENT_SECRET,
-          redirect_uri: process.env.GOOGLE_CALLBACK_DEVELOP_URL,
+          redirect_uri: process.env.GOOGLE_CALLBACK_DEPLOY_URL,
           grant_type: 'authorization_code',
         }
       );
