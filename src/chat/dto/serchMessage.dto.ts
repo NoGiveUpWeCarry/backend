07e02ec4d7b94b067d4f3537e07106c859642d67
Dong Hyun <decidere@naver.com>
2025-03-06 17:02:18 +0900

    [Feat] DTO 수정

--- src/chat/dto/serchMessage.dto.ts
+++ src/chat/dto/serchMessage.dto.ts
@@ -1,12 +1,7 @@
-import { IsNotEmpty, IsOptional } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 import { GetMessageDto } from './getMessage.dto';
-import { Type } from 'class-transformer';
 
 export class SearchMessageDto extends GetMessageDto {
-  @IsOptional()
-  @Type(() => Number)
-  cursor?: number;
-
   @IsNotEmpty({ message: '검색어를 입력해주세요' })
   keyword: any;
 }
